28b104305cc73a1716976984982fa6be9a429274
Claudi <claudi+github@grimm.link>
2024-10-07 19:09:07 +0200

    feat: update build

--- todoist.sh
+++ todoist.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
 
-# Workaround password store issue
-# Possibly https://github.com/electron/electron/issues/32598
-FLAGS='--password-store=basic'
+set -oue pipefail
 
-if [[ $XDG_SESSION_TYPE == "wayland" ]] && [ -c /dev/nvidia0 ]
-then
-    FLAGS="$FLAGS --disable-gpu-sandbox"
+EXTRA_ARGS=()
+
+add_argument() {
+    declare -i "$1"=${!1:-0}
+
+    if [[ "${!1}" -eq 1 ]]; then
+        EXTRA_ARGS+=(${@:2})
+    fi
+}
+
+# Nvidia GPUs may need to disable GPU acceleration:
+# flatpak override --user --env=TODOIST_DISABLE_GPU=1 com.todoist.Todoist
+add_argument TODOIST_DISABLE_GPU --disable-gpu
+add_argument TODOIST_ENABLE_AUTOSCROLL --enable-blink-features=MiddleClickAutoscroll
+
+# Wayland support can be optionally enabled like so:
+# flatpak override --user --socket=wayland com.todoist.Todoist
+
+WL_DISPLAY="${WAYLAND_DISPLAY:-"wayland-0"}"
+# Some compositors a real path a instead of a symlink for WAYLAND_DISPLAY:
+# https://github.com/flathub/md.obsidian.Obsidian/issues/284
+if [[ -e "${XDG_RUNTIME_DIR}/${WL_DISPLAY}" || -e "/${WL_DISPLAY}" ]]; then
+    echo "Debug: Enabling Wayland backend"
+    EXTRA_ARGS+=(
+        --ozone-platform-hint=auto
+        --enable-features=WaylandWindowDecorations
+        --enable-wayland-ime
+    )
 fi
 
-env TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID" zypak-wrapper /app/extra/todoist/todoist $FLAGS "$@"
+echo "Debug: Will run Todoist with the following arguments: ${EXTRA_ARGS[@]}"
+echo "Debug: Additionally, user gave: $@"
+
+export FLATPAK_ID="${FLATPAK_ID:-com.todoist.Todoist}"
+export TMPDIR="${XDG_RUNTIME_DIR}/app/${FLATPAK_ID}"
+
+zypak-wrapper /app/todoist/todoist $@ ${EXTRA_ARGS[@]}
